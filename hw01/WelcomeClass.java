046a05dad5a4339136b6fb2563fe81691249bcc9
Royce Kok CSE2 <rjk211@lehigh.edu>
2014-08-31 21:41:31 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+//Royce Kok
+//Hw 01
+//Welcome Class Java Prog ram
+//
+//  First compile the program
+//      javac HelloWorld.java
+//  Run the program
+//      java HelloWorld//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+public static void main(String[] args) {
+    
+// pirnt the statement with box around it
+    System.out.println("   -----------   ");
+    System.out.println("   | WELCOME |   ");
+    System.out.println("   -----------   ");
+    System.out.println(" ^  ^  ^  ^  ^  ^ ");
+    System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-R--J--K--2--1--1-|");
+    System.out.println("\\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println(" v  v  v  v  v  v  ");
+    System.out.println("This summer, I intern with a container shipping company in Hong Kong. I had a blast!");
+
+
+
+}
+}
